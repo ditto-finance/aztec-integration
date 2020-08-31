e8df774d95364852ba773c65f301171890e647cc
sunhurts <sunhurts@immutable.works>
2020-08-31 21:01:20 +0530

    migrations: fixed migrations

--- migrations/5_ZkDittoFactory.js
+++ migrations/5_ZkDittoFactory.js
@@ -4,17 +4,17 @@ const ZkDittoFactory = artifacts.require('./ZkDittoFactory.sol');
 const MockDitto = artifacts.require('./MockDitto.sol');
 
 module.exports = async (deployer, network) => {
-  await deployer.deploy(MockDitto);
-  const mockDitto = await MockDitto.deployed();
+  const aceContract = await ACE.deployed();
 
-  let aceContract;
+  // initialise the ZkDittoFactory
+  await deployer.deploy(
+    ZkDittoFactory,
+    aceContract.address
+  );
+
   if (network === 'development') {
-    aceContract = await ACE.deployed();
-    // initialise the ZkDittoFactory
-    await deployer.deploy(
-      ZkDittoFactory,
-      aceContract.address
-    );
+    await deployer.deploy(MockDitto);
+    const mockDitto = await MockDitto.deployed();
 
     const zkDittoFactory = await ZkDittoFactory.deployed();
     await zkDittoFactory.deployZkDitto(mockDitto.address);
